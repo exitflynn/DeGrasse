02aab8a4a93daa7f31b9b213ca24a507bf3efd62
Akshit Tyagi <37214399+exitflynn@users.noreply.github.com>
2023-02-05 13:00:05 +0000

    minor imprrovement

--- functions.py
+++ functions.py
@@ -16,9 +16,9 @@ async def marse(api_key):
     url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key={api_key}"
     resp = requests.get(url)
     data = json.loads(resp.text)
-    try:
+    if 'hdurl' in data:
         imgurl = data['hdurl']
-    except:
+    else:
         imgurl = data['url']
     msg_string = imgurl
     return msg_string
